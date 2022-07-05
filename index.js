fe8f3377850d01f214cdf9599ff5655d866f41d1
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2022-07-05 21:13:53 +0100

    only lint imports

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
   "extends": [
     "plugin:@typescript-eslint/recommended",
     "prettier",
+    "import-quotes"
 	],
   "settings": {
     "import/resolver": {
@@ -18,18 +19,19 @@ module.exports = {
     }
   },
   "rules": {
-    "camelcase": 2,
-    "prefer-destructuring": 2,
-    "no-nested-ternary": 2,
-    "comma-dangle": 0,
-    "@typescript-eslint/ban-ts-comment": 2,
-    "@typescript-eslint/no-explicit-any": 2,
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "@typescript-eslint/indent": 0,
-    "@typescript-eslint/no-unused-vars": 0,
-    "react/jsx-filename-extension": ["error", { "extensions": [".tsx"] }],
-    "react-hooks/rules-of-hooks": 2,
-    "react-hooks/exhaustive-deps": ["error"],
+    // "camelcase": 2,
+    // "prefer-destructuring": 2,
+    // "no-nested-ternary": 2,
+    // "comma-dangle": 0,
+    // "@typescript-eslint/ban-ts-comment": 2,
+    // "@typescript-eslint/no-explicit-any": 2,
+    // "@typescript-eslint/explicit-module-boundary-types": 0,
+    // "@typescript-eslint/indent": 0,
+    // "@typescript-eslint/no-unused-vars": 0,
+    // "react/jsx-filename-extension": ["error", { "extensions": [".tsx"] }],
+    // "react-hooks/rules-of-hooks": 2,
+    // "react-hooks/exhaustive-deps": ["error"],
+    "import-quotes/import-quotes": [1, "single"],
     "import/no-extraneous-dependencies": 2,
     "import/extensions": ["error", "never", { "svg": "always" }],
     "import/order": ["error", {"newlines-between": "always"}],
@@ -44,7 +46,7 @@ module.exports = {
     "import/no-unused-modules": 0,
     "import/no-deprecated": 0,
     "import/no-anonymous-default-export": 2,
-    "jest/no-identical-title": 2,
-    "jest/valid-expect": 2,
+    // "jest/no-identical-title": 2,
+    // "jest/valid-expect": 2,
   }
 };
